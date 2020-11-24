96e8aeddf9b3d9e3190a7defab3e501b12dc8ee9
zhang hang <josercc@163.com>
2020-11-24 22:27:11 +0800

    Initial Commit

---
+++ MySwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUIExample
+//
+//  Created by 张行 on 2020/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySwiftUIExample/MySwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIExampleApp.swift
+//  MySwiftUIExample
+//
+//  Created by 张行 on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
